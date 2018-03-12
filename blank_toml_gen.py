1cadd9f499f8d1b97fdbcc3632216bd438deae02
Jon Doe <agustusfifth@gmail.com>
2018-03-11 23:38:10 -0700

    better toml structure

--- blank_toml_gen.py
+++ blank_toml_gen.py
@@ -8,25 +8,32 @@ for lvl in range(1, 17):
     if lvl > 12:
         v2 = True
         lvl -= 4
+
+    s += "["
+    head = "level_"
+    head += str(lvl)
+    if v2:
+        head += "_v2"
+    s += head
+    s += "]\n"
     for lured in [False, True]:
-        s += "["
-        head = "level_"
-        head += str(lvl)
-        if v2:
-            head += "_v2"
-        if lured:
-            head += "_lured"
-        s += head
+        s += "  ["
+        head2 = head
+        head2 += "."
+        if not lured:
+            head2 += "non"
+        head2 += "lured"
+        s += head2
         s += "]\n"
         for org in [False, True]:
-            s += "  ["
-            s += head
+            s += "    ["
+            s += head2
             s += "."
             if not org:
                 s += "non"
             s += "org]\n"
             for gagtype in GAGS:
-                s += "    "
+                s += "      "
                 s += gagtype.ljust(max(len(g) for g in GAGS))
                 s += " = "
                 s += str([[0] * tooncount for tooncount in range(1, 5)])
